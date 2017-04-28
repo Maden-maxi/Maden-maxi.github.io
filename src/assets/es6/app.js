c1a5443a48929f528a5d5ccef23e32b1f5e2d482
Denys Dnischenko <maden.maxi@yandex.ru>
2017-04-28 21:32:54 +0300

    Blog

--- src/assets/es6/app.js
+++ src/assets/es6/app.js
@@ -1,3 +1,11 @@
 jQuery(document).ready(function($) {
-	console.log(`jQuery ${$.version}`)
+	console.log(`jQuery ${$.version}`);
+    $('.slider1').bxSlider({
+        slideWidth: 397,
+        minSlides: 1,
+        maxSlides: 3,
+        moveSlides: 1,
+        startSlide: 0,
+        slideMargin: 0
+    });
 });
